b4b909881de0d60366d11ae3b05afadc08ca3bd8
XR <xr-liujl@xr-liujl.local>
2015-10-30 13:32:35 +0800

    Initial Commit

---
+++ testSwfit1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testSwfit1
+//
+//  Created by XR on 15/10/30.
+//  Copyright © 2015年 XR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
